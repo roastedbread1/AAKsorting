c6d7b676e8b4bea7aae39e8e1e614ccd45425ecb
Al Azmi <hanifalazmi17@gmail.com>
2023-09-16 21:27:31 +0700

    first commit

--- sorting.js
+++ sorting.js
@@ -46,3 +46,23 @@ const insertionSort = (arr) => {
     }
     return arr;
 }
+
+const quickSort = (arr) => {
+    let n = arr.length;
+    if (n <=1){
+        return arr;
+    }
+
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+
+    for (let i =1; i<n; i++) {
+        if (arr[i] < pivot){
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
